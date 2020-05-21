d561fabc7cf8333e7c62efba7ac5ca1d587e3892
snail <570@snail.com>
2020-05-21 18:59:21 +0800

    21

---
+++ 父子组件传值-子组件.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{toChild}}</h1>
+    <div @click="toFather()">click me</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["toChild"],
+  methods: {
+    toFather() {
+      this.$emit("fromChild", "hello, this is from child!");
+    }
+  }
+};
+</script>
---
+++ 父子组件传值-父组件.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Child :toChild="toChild" @fromChild="getChild"></Child>
+    <h1>{{childContent}}</h1>
+  </div>
+</template>
+
+<script>
+import Child from "./components/Child.vue";
+export default {
+  data() {
+    return {
+      toChild: "hello, this is from father!",
+      childContent: "接收子组件的值！"
+    };
+  },
+  components: {
+    Child
+  },
+  methods: {
+    getChild(e) {
+      this.childContent = e;
+    }
+  }
+};
+</script>
